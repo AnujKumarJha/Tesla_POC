305894612f2226ac2cfa5dade1bbb15f69278742
sudha <sudha.gupta@952@gmail.com>
2022-07-27 19:25:25 +0800

    *Login validation Code Added

--- Tesla_POC/Validation/LoginValidation.swift
+++ Tesla_POC/Validation/LoginValidation.swift
@@ -6,3 +6,20 @@
 //
 
 import Foundation
+
+struct LoginValidation {
+    func Validate(loginRequest:LoginRequest) -> ValidationResult
+    {
+        if loginRequest.userEmail!.isEmpty
+
+        {
+            return ValidationResult(success: false, error: "User email is empty")
+        }
+        if loginRequest.userPassword!.isEmpty
+        {
+            return ValidationResult(success: false, error: "user Paswweord is empty")
+        }
+        
+        return ValidationResult(success: true, error: nil)
+    }
+}
