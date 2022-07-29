fc7b19d9e4d2d6492b7082ed143405669c7e8f63
sudha <sudha.gupta@952@gmail.com>
2022-07-29 12:50:16 +0800

    * UI Changes and MVC converted into MVVM

--- Tesla_POC/View/InsuranceDetailViewController.swift
+++ Tesla_POC/View/InsuranceDetailViewController.swift
@@ -4,80 +4,42 @@
 //
 //  Created by user218980 on 7/22/22.
 //
-struct InsuranceData:Decodable
-{
-    let home,Life,motar,Health:String
-}
-import UIKit
-class InsuranceDetailTableViewCell:UITableViewCell
-{
-    @IBOutlet weak var vwBase:UIView!
-    @IBOutlet weak var lblDetails:UILabel!
-}
-class InsuranceDetailViewController: UIViewController,UITableViewDelegate,UITableViewDataSource{
 
-    @IBOutlet weak var lblTitle:UILabel!
-    @IBOutlet weak var lblDetails:UILabel!
+import UIKit
+
+class InsuranceDetailViewController: UIViewController{
+
+    @IBOutlet weak var lblName:UILabel!
+    @IBOutlet weak var lblCompany:UILabel!
+    @IBOutlet weak var lblDescription:UILabel!
     @IBOutlet weak var btnFindQuotes:UIButton!
+    @IBOutlet weak var imgVW: UIImageView!
     
     @IBOutlet weak var tblInsuranceDetail:UITableView!
-    var navigateFrom = String()
+  
    
-    var arrDetails = [InsuranceData]()
-    
-    
+   
+
     override func viewDidLoad() {
         super.viewDidLoad()
         self.title = "Details"
-        lblTitle.text = "\(navigateFrom) Insurance"
+      
         Utilities.styleFilledButton(btnFindQuotes)
-        
-        getInsuranceDetail()
+        lblName.text = navigateFrom?.name
+        lblCompany.text = navigateFrom?.company
+        lblDescription.text = navigateFrom?.description
+        let url = URL(string: navigateFrom!.imageUrl)
+        let data = try? Data(contentsOf: url!)
+
+        if let imageData = data {
+            imgVW.image = UIImage(data: imageData)
+        }
         // Do any additional setup after loading the view.
     }
-    //MARK:- UITableView Delegate and DataSource Methods
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        
-        return arrDetails.count
-    }
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tblInsuranceDetail.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! InsuranceDetailTableViewCell
-        let dict = arrDetails[indexPath.row]
-        print(arrDetails,"dshgfdfgkufh")
-        switch navigateFrom {
-        case "Home":
-            cell.lblDetails.text = dict.home
-        case "Life":
-            cell.lblDetails.text = dict.Life
-        case "Medical":
-            cell.lblDetails.text = dict.Health
-        case "Motar":
-            cell.lblDetails.text = dict.motar
-        default:
-            break
-        }
-        return cell
-    }
+
       //MARK:- API
     
-    func getInsuranceDetail()
-    {
-    let urlString = "https://demo1273074.mockable.io/insurance"
-        let url = URL(string: urlString)!
-        let apiController = HttpUtility()
-        apiController.getApiData(requestUrl: url, resultType: [InsuranceData].self) { result in
-            if result != nil
-            {
-                print(result as Any)
-                self.arrDetails = result!
-                DispatchQueue.main.async {
-                    self.tblInsuranceDetail.reloadData()
-                }
-               
-            }
-        }
-    }
-    
+
     @IBAction func btnFindQuotation(_ sender: UIButton) {
         let calculateQuoVC = self.storyboard?.instantiateViewController(withIdentifier: "CalculateQuotesViewController") as! CalculateQuotesViewController
         self.navigationController?.pushViewController(calculateQuoVC, animated: true)
